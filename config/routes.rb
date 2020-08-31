d17fa06972702fc21e5481211839aa6fb6e2fa8b
Layne Rasmussen <layrasm@gmail.com>
2020-08-31 17:41:55 -0600

    hobby model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'homes/index'
+  get 'hobbies/index'
+  get 'hobbies/show'
+  get 'hobbies/new'
+  get 'hobbies/edit'
   root "homes#index"
   devise_for :users
 
-  resources :homes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_200658) do
+ActiveRecord::Schema.define(version: 2020_08_31_233803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "hobbies", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_hobbies_on_user_id"
+  end
 
   create_table "homes", force: :cascade do |t|
     t.string "index"
@@ -35,5 +43,6 @@ ActiveRecord::Schema.define(version: 2020_08_31_200658) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "hobbies", "users"
   add_foreign_key "homes", "users"
 end
