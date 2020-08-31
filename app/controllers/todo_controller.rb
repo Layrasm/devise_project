d1e31f9c8710b165462d4b50ca347230bfbd21a6
Victor Gervacio <victorgervacio33@gmail.com>
2020-08-31 17:17:28 -0600

    working on new

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -8,8 +8,31 @@ class TodoController < ApplicationController
   end
 
   def new
+    @todo = Todo.create
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if todo.save
+      redirect_to @todo
+    else
+      render :new
+    end
+  end
+  def destroy 
+   @todo.destroy 
+   redirect_to todo_index_path
   end
 
   def edit
+  end
+
+  private 
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+  def todo_params
+    params.requiere(:todo).permit(:name, :note)
   end
 end
