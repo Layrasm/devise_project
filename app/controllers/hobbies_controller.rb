e238e463846c81d643b54dcaca16519395790de8
Layne Rasmussen <layrasm@gmail.com>
2020-09-01 08:06:03 -0600

    hobby views and controller finished, added sign out button to home page

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -1,13 +1,38 @@
 class HobbiesController < ApplicationController
+  before_action :set_hobby, only: [:show, :destroy]
+  
   def index
+    @hobbies = current_user.hobbies
   end
 
   def show
   end
 
   def new
+    @hobby = Hobby.new
   end
 
-  def edit
+  def create
+    @hobby = current_user.hobbies.new(hobby_params)
+
+    if @hobby.save
+      redirect_to @hobby
+    else
+      render :new
+    end
   end
+
+  def destroy
+    @hobby.destroy
+    redirect_to hobbies_path
+  end
+
+  private
+    def set_hobby
+      @hobby = Hobby.find(params[:id])
+    end
+
+    def hobby_params
+      params.require(:hobby).permit(:name)
+    end
 end
